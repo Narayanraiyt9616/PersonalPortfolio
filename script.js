e48f015ba03e0bc620e780d55a76086b65a1470a
Narayan Rai <rainarayan709@gmail.com>
2026-04-25 11:59:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.getElementsByClassName("about");
+
+// function about (){
+//     // onclick()="http://127.0.0.1:5500/about.html"
+// }
+
+let paragraph= document.getElementsByTagName("h1")
